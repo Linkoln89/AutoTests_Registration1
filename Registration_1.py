71f57e7f6bc646530e2474d4c2137bd4fa4c655a
Andrei Lomako <Linkoln89@live.com>
2018-09-21 18:03:31 +0300

    1st step of registration
    Without phone and email autogeneration

---
+++ Registration_1.py
@@ -0,0 +1,29 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("https://moneyman:1005@qa-delivery-br-master.moneyman.ru/registration-gf/static/#/step1")
+        
+        elem = driver.find_element_by_name("name")
+        elem.send_keys("Demo Demosson")
+        
+        elem = driver.find_element_by_name("mothersName")
+        elem.send_keys("Demo Demossona")
+        
+        elem = driver.find_element_by_name("phone")
+        elem.send_keys("10925545432")
+        
+        elem = driver.find_element_by_name("email")
+        elem.send_keys("tetete777777@mail.ru")
+        
+        elem = driver.find_element_by_name ('nextBtn'). click()
+
+if __name__ == "__main__":
+    unittest.main()
